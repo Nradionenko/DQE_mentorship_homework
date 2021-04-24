fe2ee731638fddf625c42126eec19455f5a9025b
Nadia Radionenko <n.radionenko@gmail.com>
2021-04-25 01:50:43 +0300

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,11 +1,11 @@
 import pyodbc
 from os.path import dirname, join, normcase
 
-from exec_utils.configloader import Config
+# from exec_utils.configloader import Config
 from results.logging import Log
 
-cnf = Config()
-log = Log()
+# cnf = Config()
+# log = Log()
 
 
 class DB_CONNECT:
@@ -19,38 +19,42 @@ class DB_CONNECT:
                                   'Database={TRN};'
                                   'Trusted_Connection=no;') as conn:
                 curs = conn.cursor()
-            log.connected(server_name, db_name)
+#             log.connected(server_name, db_name)
             return curs
         except pyodbc.Error as e:
-            log.log_error(e)
+            print(e)
+#             log.log_error(e)
 
-    def read_query(self, query_file):
-        main_dir = normcase(dirname(__file__))
-        query_file_path = join(main_dir, query_file)
-        try:
-            with open(query_file_path, "r") as f:
-                log.query_start(query_file)
-                return f.read()
-        except FileNotFoundError as e:
-            log.log_error(e)
+#     def read_query(self, query_file):
+#         main_dir = normcase(dirname(__file__))
+#         query_file_path = join(main_dir, query_file)
+#         try:
+#             with open(query_file_path, "r") as f:
+#                 log.query_start(query_file)
+#                 return f.read()
+#         except FileNotFoundError as e:
+#             log.log_error(e)
 
-    def run_query(self, query_file):
-        try:
-            curs = self.connect()
-            query = self.read_query(query_file)
-            curs.execute(query)
-            result = curs.fetchall()[0][0]
-            for i in result:
-                print(i)
-            log.query_finish(query_file)
-            curs.close()
-            return result
-        except AttributeError:
-            log.log_error("Couldn't connect to the server")
-        except pyodbc.ProgrammingError as p:
-            log.log_error(p)
+#     def run_query(self, query_file):
+#         try:
+#             curs = self.connect()
+#             query = self.read_query(query_file)
+#             curs.execute(query)
+#             result = curs.fetchall()[0][0]
+#             for i in result:
+#                 print(i)
+#             log.query_finish(query_file)
+#             curs.close()
+#             return result
+#         except AttributeError:
+#             log.log_error("Couldn't connect to the server")
+#         except pyodbc.ProgrammingError as p:
+#             log.log_error(p)
 
 
 d = DB_CONNECT()
-d.connect()
-d.run_query(cnf.get_val("QUERIES", 'query_01'))
+curs = d.connect()
+curs.execute('select * from trn.regions')
+result = curs.fetchall()
+for i in result:
+    print(i)
